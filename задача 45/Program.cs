43c511dcd1a88b7b231d24403b1842a7844f54ae
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-08 15:23:36 +0300

    выполнили задачу 45

--- задача 45/Program.cs
+++ задача 45/Program.cs
@@ -1,2 +1,28 @@
 ﻿// Напишите программу, которая будет создавать копию заданного массива с помощью элементного копирования
 
+int[] GetArray() {
+    int[] Array = new int[5];
+    for (int i = 0; i < Array.Length; i++)
+    {
+     Array[i] = new Random().Next(1, 10);   
+    }
+    return Array;
+}
+
+int[] CopyArray(int[] array) {
+    int[] NewArray = new int[array.Length];
+    for (int i = 0; i <array.Length; i++)
+    {
+        NewArray[i] = array[i];
+    }
+    return NewArray; 
+
+}
+
+void Main() {
+    int[] ArrayOld = GetArray();
+    int[] NewArray = CopyArray(ArrayOld);
+    System.Console.WriteLine(String.Join(", ", ArrayOld));
+    System.Console.WriteLine(String.Join(", ", NewArray));
+}
+Main();
