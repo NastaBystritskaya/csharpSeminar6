69fa22dc729473e091feed9413ba4f4e1f494d38
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-08 14:21:35 +0300

    Выполнили задачу 42

--- задача 42/Program.cs
+++ задача 42/Program.cs
@@ -3,4 +3,20 @@
 // 3 -> 11
 // 2 -> 10 
 
+int GetUserInput() {
+    System.Console.WriteLine("Введите число: ");
+    int Input = int.Parse(System.Console.ReadLine()!);
+    return Input;
+}
 
+void PrintBinary(int input) {
+    string Result = Convert.ToString(input, 2);
+    System.Console.WriteLine(Result);
+}
+
+void Main() {
+    int UserInput = GetUserInput();
+    PrintBinary(UserInput);
+}
+
+Main();
