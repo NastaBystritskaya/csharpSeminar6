3f194dd6a1d77c7b806ab9639f5209f7f9d81a65
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-08 15:12:05 +0300

    выполнили задачу 44

--- задача 44/Program.cs
+++ задача 44/Program.cs
@@ -2,3 +2,27 @@
 // Если N = 5 -> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
+
+int GetUserInput() {
+    System.Console.WriteLine("Введите число: ");
+    int Input = int.Parse(System.Console.ReadLine()!);
+    return Input;
+}
+
+void PrintFibonache(int input) {
+    System.Console.Write("0 1 ");
+    for (int i = 1; i < input - 1; i++) {
+        if(i == 1 || i==2) {
+          System.Console.Write(i + " ");  
+        } else {
+          System.Console.Write(((i-1) + (i - 2)) + " ");
+        }
+    }
+}
+
+void Main() {
+    int UserInput = GetUserInput();
+    PrintFibonache(UserInput);
+}
+
+Main();
